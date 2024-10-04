bf8e071e533242d3ed8779e4093cbb005d0778de
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-04 14:05:54 +0900

    [ :wrench: | Fix ] festivalCardStore.js JSON.parse 제거

--- src/store/festivalCardStore.jsx
+++ src/store/festivalCardStore.jsx
@@ -1,8 +1,8 @@
 import { create } from "zustand";
 
 const useFestivalCardStore = create((set) => ({
-  festivalCards: JSON.parse(localStorage.getItem("festivalCards")) || [],
-  selectedAreaCode: JSON.parse(localStorage.getItem("selectAreaCode")) || null,
+  festivalCards: localStorage.getItem("festivalCards") || [],
+  selectedAreaCode: localStorage.getItem("selectAreaCode") || null,
   setFestivalCards: (cards) => {
     set({ festivalCards: cards });
     localStorage.setItem("festivalCards", JSON.stringify(cards));
