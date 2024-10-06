3a470e100123c3d1a0ec150d316cb7086cb9bce0
asdfghjk1052@naver.com <asdfghjk1052@naver.com>
2024-10-06 22:53:31 +0900

    [ :sparkles: | #7 | Feature ] 찜 기능, 초기 화면 전국 데이터 띄우기 완료

--- src/store/festivalWishStore.jsx
+++ src/store/festivalWishStore.jsx
@@ -1,12 +1,17 @@
 import { create } from "zustand";
 
 export const useFestivalWishStore = create((set) => ({
-  wishList: {},
-  toggleWish: (contentid) =>
-    set((state) => ({
-      wishList: {
-        ...state.wishList,
-        [contentid]: !state.wishList[contentid],
-      },
-    })),
+  wishList: JSON.parse(localStorage.getItem("wishList")) || {},
+  toggleWish: (contentid) => {
+    set((state) => {
+      const updateWishList = { ...state.wishList };
+      if (updateWishList[contentid]) {
+        delete updateWishList[contentid];
+      } else {
+        updateWishList[contentid] = true;
+      }
+      localStorage.setItem("wishList", JSON.stringify(updateWishList));
+      return { wishList: updateWishList };
+    });
+  },
 }));
